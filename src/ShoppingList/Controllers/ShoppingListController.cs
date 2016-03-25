2870f3f0f6132c786332bd227dd68424d7adb568
David Rundqvist <david.rundqvist@gmail.com>
2016-03-25 13:29:09 +0100

    Adding first GUI

---
+++ src/ShoppingList/Controllers/ShoppingListController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace ShoppingList.Controllers
+{
+    public class ShoppingListController : Controller
+    {
+        public IActionResult ViewShoppingLists()
+        {
+            ViewData["Message"] = "Your application description page.";
+            return View();
+        }
+
+        public IActionResult EditShoppingList()
+        {
+            return View();
+        }
+
+        public IActionResult GoShopping()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/ShoppingList/Controllers/StoreController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace ShoppingList.Controllers {
+    public class StoreController : Controller
+    {
+        public IActionResult ViewStores()
+        {
+            return View();
+        }
+    }
+}
