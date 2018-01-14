6fd641484ea307c1cc5af37384e0302146080edc
David Rundqvist <david.rundqvist@gmail.com>
2018-01-14 10:46:35 +0100

    Changed background colors i edit and shopping pages

--- Publish-ToBraavos.ps1
+++ Publish-ToBraavos.ps1
@@ -1,1 +1,11 @@
-﻿dotnet publish .\src\ShoppingList -o "\\BRAAVOS\asp\ShoppingList" --configuration Release -r ubuntu.14.04-x64
+﻿$ErrorActionPreference = "Stop"
+
+rename-item "\\BRAAVOS\asp\ShoppingList" "\\BRAAVOS\asp\ShoppingList_old"
+
+"Publishing new..."
+dotnet publish .\src\ShoppingList -o "\\BRAAVOS\asp\ShoppingList" --configuration Release -r ubuntu.14.04-x64
+
+"Removing old..."
+del "\\BRAAVOS\asp\ShoppingList_old" -Recurse -Force
+
+write-host "Shopping list published" -ForegroundColor Green
