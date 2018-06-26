b36293868f4d1ab6cd92bd6ea7d4b07e72eb692d
David Rundqvist <david.rundqvist@gmail.com>
2018-06-26 13:54:51 +0200

    Modified publish script to stop & start the service via ssh

--- Publish-ToBraavos.ps1
+++ Publish-ToBraavos.ps1
@@ -1,11 +1,26 @@
 ﻿$ErrorActionPreference = "Stop"
 
-rename-item "\\BRAAVOS\asp\ShoppingList" "\\BRAAVOS\asp\ShoppingList_old"
+pushd $PSScriptRoot
 
-"Publishing new..."
+# Stop previous version
+Write-Host -foreground Cyan "Stopping old version..."
+ssh -i .\davidAtBraavos david@braavos "killall dotnet"
+
+
+# Publish new
+Write-Host -foreground Cyan "Publishing new version..."
+rename-item "\\BRAAVOS\asp\ShoppingList" "\\BRAAVOS\asp\ShoppingList_old"
 dotnet publish .\src\ShoppingList -o "\\BRAAVOS\asp\ShoppingList" --configuration Release -r ubuntu.14.04-x64
 
-"Removing old..."
-del "\\BRAAVOS\asp\ShoppingList_old" -Recurse -Force
+#Start new
+Write-Host -foreground Cyan "Starting new version..."
+$startCommand = "cd /home/david/asp/ShoppingList/
+nohup dotnet ShoppingList.dll > /dev/null 2>&1 &"
+ssh -i .\davidAtBraavos $startCommand
 
-write-host "Shopping list published" -ForegroundColor Green
+# Cleanup
+Write-Host -foreground Cyan "Removing old version..."
+del "\\BRAAVOS\asp\ShoppingList_old" -Recurse -Force
+popd
+
+Write-Host -ForegroundColor Green "Shopping list published" 
