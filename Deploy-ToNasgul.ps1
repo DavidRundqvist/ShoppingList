ad955367dd41e9e9dc7bd3c7cce81d64a217db44
David Rundqvist <david.rundqvist@gmail.com>
2025-08-10 21:24:38 +0200

    Readded docker file
    This reverts commit 0863b6f3d568e03d4e7ea93af18011b1d0a60fda.
    
    # Conflicts:
    #	src/ShoppingList/Properties/PublishProfiles/DoDShopping - Web Deploy.pubxml.user
    #	src/ShoppingList/ShoppingList.csproj

---
+++ Deploy-ToNasgul.ps1
@@ -0,0 +1,16 @@
+# Copy files
+$slFolder = Resolve-Path $PSScriptRoot\src\ShoppingList
+$binFolder = Resolve-Path $slFolder\bin
+$objFolder = Resolve-Path $slFolder\obj
+$targetFolder = "\\nasgul\David\ShoppingList"
+robocopy $slFolder $targetFolder /MIR /XD $binFolder $objFolder
+
+# Build and run docker image
+$dockerPath = "/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker"
+$dockerRmCmd = "$dockerPath rm -f shoppinglist;sleep 3"
+$gotoFolderCmd = "cd /share/David/ShoppingList"
+$dockerBuildCmd = "$dockerPath build -t shoppinglist ."
+$dockerRunCmd = "$dockerPath run --restart always -d -p 5000:5000 --name shoppinglist -v /share/David/ShoppingListData:/data shoppinglist"
+$allCommands = "$dockerRmCmd ; $gotoFolderCmd ; $dockerBuildCmd ; $dockerRunCmd"
+ssh -o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedAlgorithms=+ssh-rsa -o MACs=hmac-sha2-256 david@nasgul $allCommands 
+ 
